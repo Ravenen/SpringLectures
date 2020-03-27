36649bc23c4c329a905ced94cc7568dbe345cd7b
Raven <redstoyningbros@gmail.com>
2020-03-27 10:08:30 +0200

    Webinar 27.03

--- src/main/java/ua/lviv/iot/spring/first/rest/RestApplication.java
+++ src/main/java/ua/lviv/iot/spring/first/rest/RestApplication.java
@@ -7,11 +7,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan({ "ua.lviv.iot.spring.first.dataaccess", "ua.lviv.iot.spring.first.business",
-    "ua.lviv.iot.spring.first.rest.controller" })
+    "ua.lviv.iot.spring.first.rest.controller", "ua.lviv.iot.spring.first.rest" })
 @EnableJpaRepositories("ua.lviv.iot.spring.first.dataaccess")
 public class RestApplication {
 
   public static void main(String[] args) {
+    //System.setProperty("java.awt.headless", "false");
     SpringApplication.run(RestApplication.class, args);
   }
 
